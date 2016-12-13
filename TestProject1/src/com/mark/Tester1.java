77771404e7d30f8a52ca8b42a92f222576631755
Mark Pergamenshik <markp@callup.net>
2016-12-13 16:54:13 +0200

    Initial check in

---
+++ TestProject1/src/com/mark/Tester1.java
@@ -0,0 +1,10 @@
+package com.mark;
+
+public class Tester1 {
+
+	public static void main(String[] args) {
+		System.out.println("Tester 1");
+
+	}
+
+}
---
+++ TestProject1/src/com/mark/Tester2.java
@@ -0,0 +1,10 @@
+package com.mark;
+
+public class Tester2 {
+
+	public static void main(String[] args) {
+		System.out.println("Tester 2");
+
+	}
+
+}
