ddf6d3ef62fc37fcae0350fa5b54f35ee504af84
Mark Pergamenshik <markp@callup.net>
2016-12-14 10:27:00 +0200

    Still Testing

--- TestProject1/src/com/mark/Tester1.java
+++ TestProject1/src/com/mark/Tester1.java
@@ -6,6 +6,12 @@ public class Tester1 {
 		System.out.println("Tester 1");
 		System.out.println("Another Tester 1");
 		System.out.println("Another Tester 2");
+		System.out.println("Another Tester 3");
+		System.out.println("Another Tester 4");
+		System.out.println("Another Tester 5");
+		System.out.println("Another Tester 6");
+		System.out.println("Another Tester 7");
+		System.out.println("Another Tester 8");
 	}
 
 }
