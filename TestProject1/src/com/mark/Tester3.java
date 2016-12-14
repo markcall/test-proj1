fb13cb5160d50060c965e2914bd718c56f9ea187
Marks Virtual machine <IEUser@10.0.2.15>
2016-12-14 00:15:02 -0800

    Adding some changes

--- TestProject1/src/com/mark/Tester3.java
+++ TestProject1/src/com/mark/Tester3.java
@@ -4,6 +4,12 @@ public class Tester3 {
 
 	public static void main(String[] args) {
 		System.out.println("Tester 3");
+		System.out.println("Tester 3");
+		System.out.println("Tester 3");
+		System.out.println("Tester 3");
+		System.out.println("Tester 3");
+		System.out.println("Tester 3");
+		
 
 	}
 
